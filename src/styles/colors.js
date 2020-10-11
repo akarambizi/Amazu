073748ba0c3805c0d5f4249ff42a71ca05ecd8c3
Arthur Karambizi <arthurkarambizi@gmail.com>
2020-10-10 23:01:30 -0400

    added fonts, colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,12 +1,20 @@
 const colors = {
-    green: '#388E3C',
-    lightgreen: '#4CAF50',
-    primaryLight: '#C8E6C9',
-    white: '#FFFFFF',
-    accent: '#009688',
-    text: '#212121',
-    textSecondary: '#757575',
-    divider: '#BDBDBD',
+    '--swatch_1': '#eff3f2',
+    '--swatch_2': '#96c8c7',
+    '--swatch_3': '#73aaa9',
+    '--swatch_4': '#b2ccb0',
+    '--swatch_5': '#30372d',
+    '--swatch_6': '#92b2ae',
+    '--swatch_7': '#6ec6a7',
+    '--swatch_8': '#aa9f67',
+    '--swatch_9': '#f6f9f8',
+    '--swatch_10': '#9bcac8',
+    '--swatch_11': '#8cb7b7',
+    '--swatch_12': '#2d4736',
+    '--swatch_13': '#a1cab7',
+    '--swatch_14': '#71a9a5',
+    '--swatch_15': '#6dc6a5',
+    '--swatch_16': '#869652',
 };
 
 export default colors;
---
+++ src/styles/fonts.js
@@ -0,0 +1,7 @@
+const fonts = {
+    '--font-primary': "'Montserrat', sans-serif",
+    '--font-secondary': "'Open Sans', sans-serif",
+    '--font-tertiary': "'Roboto', sans-serif;",
+};
+
+export default fonts;
