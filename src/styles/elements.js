908fe0a4be43c58d5cd299bd14d779fb0327ba7e
Arthur Karambizi <arthurkarambizi@gmail.com>
2020-11-06 23:27:51 -0500

    made changes to the elements

--- src/styles/elements.js
+++ src/styles/elements.js
@@ -6,19 +6,18 @@ export const Wrapper = styled.div`
     padding: 0 20px;
 
     ${media.laptop} {
-        max-width: 1240px;
         margin: 0 auto;
+        max-width: 1240px;
         padding: 0;
     }
 `;
 
 export const Grid = styled.section`
     display: flex;
+    flex-wrap: wrap;
+    display: grid;
     align-items: center;
     gap: 0 24px;
-    flex-wrap: wrap;
-
-    display: grid;
     grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
 `;
 
@@ -28,58 +27,56 @@ const titles = {
     h4: 'font-size: 24px;',
     h5: 'font-size: 20px;',
     h6: 'font-size: 18px;',
-    p: 'font-size: 22px;',
+    p: 'font-size: 22px;'
 };
 
 export const Title = styled.h1`
     ${(props) => (props.as in titles ? titles[props.as] : 'font-size: 50px;')};
     font-family: var(--font-tertiary);
+    font-weight: 700;
     letter-spacing: 0;
     line-height: 30px;
     margin-bottom: 10px;
-    font-weight: 700;
 `;
 
 export const Text = styled.p`
-    font-family: var(--font-tertiary);
-    font-size: ${(props) => (props.bold ? 'var(--font-size-xl)' : 'var(--font-size-md)')};
-    line-height: 24px;
-    font-weight: ${(props) => props.bold && '700'};
     color: ${(props) => (props.secondary ? 'var(--primary-color)' : 'var(--primary-text-color)')};
     display: ${(props) => props.display};
-    width: ${(props) => props.width};
+    font-family: var(--font-tertiary);
+    font-size: ${(props) => (props.bold ? 'var(--font-size-xl)' : 'var(--font-size-md)')};
+    font-weight: ${(props) => props.bold && '700'};
+    line-height: 24px;
     margin-bottom: 10px;
+    width: ${(props) => props.width};
 `;
 
 export const Anchor = styled.a`
-    text-transform: capitalize;
-    text-decoration: none !important;
     display: block;
+    text-decoration: none !important;
+    text-transform: capitalize;
 `;
 
 export const Button = styled.button`
-    cursor: pointer;
     background: ${(props) => (props.secondary ? 'var(--primary-color-text)' : 'var(--primary-color)')};
-    box-shadow: ${(props) => (props.shadow ? '0px 10px 24px rgba(0, 0, 0, 0.19)' : null)};
     border-radius: 5px;
+    border: ${(props) => (props.secondary ? '1px solid var(--divider-color)' : '1px solid var(--primary-color)')};
+    box-shadow: ${(props) => (props.shadow ? '0px 10px 24px rgba(0, 0, 0, 0.19)' : null)};
+    color: ${(props) => (props.secondary ? 'var(--primary-text-color)' : 'var(--primary-color-text)')};
+    cursor: pointer;
+    display: ${(props) => (props.min ? 'inline-block' : 'block')};
     font-family: var(--font-secondary);
-    font-style: normal;
-    font-weight: normal;
     font-size: var(--font-size-sm);
     line-height: 16px;
-    color: ${(props) => (props.secondary ? 'var(--primary-text-color)' : 'var(--primary-color-text)')};
-    border: ${(props) => (props.secondary ? '1px solid var(--divider-color)' : '1px solid var(--primary-color)')};
-    padding: 14px 20px;
     min-width: 142px;
-    width: ${(props) => props.width};
+    padding: 14px 20px;
     text-transform: capitalize;
     transition: 0.3s;
-    display: ${(props) => (props.min ? 'inline-block' : 'block')};
+    width: ${(props) => props.width};
 
     &:hover {
         background: var(--primary-color-light);
-        color: var(--primary-text-color);
         border: 1px solid var(--primary-color-light);
+        color: var(--primary-text-color);
     }
 `;
 
@@ -90,16 +87,16 @@ export const List = styled.ul`
 export const ListItem = styled.li`
     font-family: var(----font-primary);
     font-size: 14px;
-    line-height: 26px;
     letter-spacing: 0;
+    line-height: 26px;
 
     &::before {
         ${mixins.inlineBlock}
+        background-color: var(--primary-color);
         border-radius: 50%;
         content: '';
         height: 6px;
-        width: 6px;
         margin-right: 7px;
-        background-color: var(--primary-color);
+        width: 6px;
     }
 `;
