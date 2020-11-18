0e0c518c6f46b0bf29b78b6a89678766068bd848
Arthur Karambizi <arthurkarambizi@gmail.com>
2020-11-17 23:39:29 -0500

    made changes to the register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { Title } from '../styles/elements';
+import { Title, Text} from '../styles/elements';
 import { mixins } from '../styles';
 
 const RegisterMain = styled.section`
@@ -9,21 +9,34 @@ const RegisterMain = styled.section`
     /* background-color: red; */
     height: 100vh;
     form {
-        width: 100%;
+        width: 40%;
+        padding: 0 40px;
     }
 
     div {
-        color: white;
-        height: 100%;
-        width: 100%;
+        ${mixins.flexColumn}
+        justify-content:center;
+        background-color: green;
         background-image: url('https://images.unsplash.com/photo-1542687226-54a9a41eeafd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80');
+        background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
-        background-position: center;
-        background-color: green;
+        color: var(--primary-color-text);
+        height: 100%;
+        text-align: center;
+        width: 60%;
         ul {
             li {
                 ${mixins.inlineBlock}
+                margin:0 24px;
+                a {
+                    color: var(--primary-color-text);
+                    font-size: 18px;
+
+                    &:hover {
+                        color: var(--primary-color);
+                    }
+                }
             }
         }
     }
@@ -48,13 +61,17 @@ function Register() {
             </div>
             <form action="">
                 <label htmlFor="userName">Username</label>
-                <input type="text" name="userName" id="userName" placeholder="username" />
+                <input type="text" name="userName" id="userName" placeholder="Username" />
                 <label htmlFor="userName">Password</label>
-                <input type="password" name="password" id="password" placeholder="password" autoComplete="on" />
+                <input type="password" name="password" id="password" placeholder="Password" autoComplete="on" />
                 <button type="submit">Register</button>
                 <label htmlFor="rememberMe">Remember me</label>
                 <input type="checkbox" name="rememberMe" id="rememberMe" />
                 <Link to="/">Forgot Password?</Link>
+                <Text>
+                    Don’t have an account?
+                    <Link to="/">Sign up ?</Link>
+                </Text>
             </form>
         </RegisterMain>
     );
