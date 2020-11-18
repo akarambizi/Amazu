9bc90735da212628abd3c9a31d39e803e9dacb54
Arthur Karambizi <arthurkarambizi@gmail.com>
2020-11-17 23:39:44 -0500

    made changes to the register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { Title, Text} from '../styles/elements';
+import { Title, Text } from '../styles/elements';
 import { mixins } from '../styles';
 
 const RegisterMain = styled.section`
