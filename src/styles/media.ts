a3cd5f99fd2dc436e0151a315e67fafd83725721
Arthur Karambizi <arthurkarambizi@gmail.com>
2023-01-20 02:14:09 -0800

    added eslintrc

--- src/styles/media.ts
+++ src/styles/media.ts
@@ -1,3 +1,19 @@
-export { default as media } from './media';
-export { default as GlobalStyles } from './GlobalStyles';
-export { default as mixins } from './mixins';
+const size = {
+  mobile: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  desktop: "1440px",
+};
+
+const media = {
+  mobile: `@media (min-width: ${size.mobile})`,
+  mobileMax: `@media (max-width: ${size.mobile})`,
+  tablet: `@media (min-width: ${size.tablet})`,
+  tabletMax: `@media (max-width: ${size.tablet})`,
+  laptop: `@media (min-width: ${size.laptop})`,
+  laptopMax: `@media (max-width: ${size.laptop})`,
+  desktop: `@media (min-width: ${size.desktop})`,
+  desktopMax: `@media (max-width: ${size.desktop})`,
+};
+
+export default media;
