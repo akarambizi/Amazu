f032fb1ea9cff72ce12b110f4acb8a8e78283d97
Arthur Karambizi <arthurkarambizi2@gmail.com>
2023-02-07 23:42:12 -0800

    added QueryClientProvider in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home, Rent, Buy, NotFound, Typography, PropertyPage, SignUp, SignIn, SearchResults } from './pages';
 import { Footer, Header } from './components';
 import { GlobalStyles } from './styles';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
     useEffect(() => {
@@ -10,24 +13,26 @@ const App = () => {
     }, []);
 
     return (
-        <Router>
-            <GlobalStyles />
-            <Header location={{ pathname: 'home' }} />
-            <main>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/rent" element={<Rent />} />
-                    <Route path="/buy" element={<Buy />} />
-                    <Route path="/typography" element={<Typography />} />
-                    <Route path="/property" element={<PropertyPage />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route path="/signin" element={<SignIn />} />
-                    <Route path="/search" element={<SearchResults />} />
-                    <Route element={<NotFound />} />
-                </Routes>
-            </main>
-            <Footer location={{ pathname: 'home' }} />
-        </Router>
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <GlobalStyles />
+                <Header location={{ pathname: 'home' }} />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/rent" element={<Rent />} />
+                        <Route path="/buy" element={<Buy />} />
+                        <Route path="/typography" element={<Typography />} />
+                        <Route path="/property" element={<PropertyPage />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/signin" element={<SignIn />} />
+                        <Route path="/search" element={<SearchResults />} />
+                        <Route element={<NotFound />} />
+                    </Routes>
+                </main>
+                <Footer location={{ pathname: 'home' }} />
+            </Router>
+        </QueryClientProvider>
     );
 };
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
+        <App />
     </React.StrictMode>
 );
 
