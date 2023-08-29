609e6ababef4b648831ae785dfa43fcbb46f4d8b
Arthur Karambizi <arthurkarambizi2@gmail.com>
2023-08-28 23:39:32 -0700

    added useCitiesData hook

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { IProperty } from './model/property';
+import { IProperty, ICity } from './model';
 
 export const getUrl = (path: string, environment?: string) => `http://localhost:4200${path}`;
 
@@ -10,6 +10,17 @@ export const getUrl = (path: string, environment?: string) => `http://localhost:
  */
 export const getPropertiesData = async (query = ''): Promise<IProperty[]> => {
     const url = getUrl('/properties');
+    const response = await axios.get(`${url}${query}`);
+    return response?.data ?? [];
+};
+
+/**
+ * Gets cities data.
+ * @param {string} query - The search query string.
+ * @returns {Promise<IProperty[]>}  array of cities results.
+ */
+export const getCitiesData = async (query = ''): Promise<ICity[]> => {
+    const url = getUrl('/cities');
     const response = await axios.get(`${url}${query}`);
     return response?.data ?? [];
 };
--- src/api/queries/index.ts
+++ src/api/queries/index.ts
@@ -1,9 +1,10 @@
 import { UseQueryOptions, useQuery } from 'react-query';
-import { getPropertiesData } from '..';
+import { getCitiesData, getPropertiesData } from '..';
 import { AxiosError } from 'axios';
-import { IProperty } from '../model/property';
+import { IProperty, ICity } from '../model';
 
 const getSearchResultsKey = (query: string) => ['searchResultsData', query];
+const getCitiesKey = (query: string) => ['citiesData', query];
 
 /**
  * A custom hook that fetches search results data.
@@ -15,6 +16,22 @@ export const usePropertiesData = (query: string, options?: UseQueryOptions<IProp
     return useQuery({
         queryKey: getSearchResultsKey(query),
         queryFn: () => getPropertiesData(query),
+        refetchOnWindowFocus: false,
+        retry: false,
+        ...options,
+    });
+};
+
+/**
+ * A custom hook that fetches cities  data.
+ * @param {string} query - The search query string.
+ * @param [options] - Optional options for the `useQuery` hook.
+ * @returns {QueryObserverResult<ICity[], AxiosError>} The result of the `useQuery` hook.
+ */
+export const useCitiesData = (query: string, options?: UseQueryOptions<ICity[], AxiosError, ICity[], ReturnType<typeof getSearchResultsKey>>) => {
+    return useQuery({
+        queryKey: getCitiesKey(query),
+        queryFn: () => getCitiesData(query),
         refetchOnWindowFocus: false,
         retry: false,
         ...options,
