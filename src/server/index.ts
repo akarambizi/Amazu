14654230133b3c9fcdc260fd1fa55753e91d6f56
Arthur Karambizi <arthurkarambizi2@gmail.com>
2023-09-12 00:18:31 -0700

    added server folder

---
+++ src/server/index.ts
@@ -0,0 +1,21 @@
+// import express from 'express';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
