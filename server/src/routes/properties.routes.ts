d4b7e70db2a181cddd20fcb60dc93cb4df73a664
Arthur Karambizi <arthurkarambizi2@gmail.com>
2023-12-10 04:00:23 -0800

    Refactor Swagger documentation in properties.routes.ts

--- server/src/routes/properties.routes.ts
+++ server/src/routes/properties.routes.ts
@@ -3,77 +3,8 @@ import { propertiesController } from '../controllers';
 
 export const propertiesRoutes = express.Router();
 
-/**
- * @swagger
- * /properties:
- *   get:
- *     tags: ['Properties']
- *     summary: Retrieve a list of properties
- *     responses:
- *       200:
- *         description: A list of properties.
- */
 propertiesRoutes.get('/', propertiesController.getAllProperties);
-
-/**
- * @swagger
- * /properties:
- *   post:
- *     tags: ['Properties']
- *     summary: Create a new property
- *     responses:
- *       201:
- *         description: The created property.
- */
 propertiesRoutes.post('/', propertiesController.createProperty);
-
-/**
- * @swagger
- * /properties/{id}:
- *   get:
- *     tags: ['Properties']
- *     summary: Retrieve a property by its ID
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: Property ID
- *     responses:
- *       200:
- *         description: The requested property.
- */
 propertiesRoutes.get('/:id', propertiesController.getProperty);
-
-/**
- * @swagger
- * /properties/{id}:
- *   patch:
- *     tags: ['Properties']
- *     summary: Update a property by its ID
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: Property ID
- *     responses:
- *       200:
- *         description: The updated property.
- */
 propertiesRoutes.patch('/:id', propertiesController.updateProperty);
-
-/**
- * @swagger
- * /properties/{id}:
- *   delete:
- *     tags: ['Properties']
- *     summary: Delete a property by its ID
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: Property ID
- *     responses:
- *       200:
- *         description: The deleted property.
- */
 propertiesRoutes.delete('/:id', propertiesController.deleteProperty);
--- server/swagger.ts
+++ server/swagger.ts
@@ -1,21 +1,4 @@
-import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-
-const port = process.env.PORT || '8000';
-
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: 'Amazu API',
-            description: 'This is a simple CRUD API application made with Express and documented with Swagger',
-            version: '0.1.0',
-            servers: [`http://localhost:${port}`],
-        },
-    },
-    // Path to your API routes
-    apis: ['./src/routes/*.ts'],
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+import swaggerDocs from './swagger.json';
 
 export { swaggerUi, swaggerDocs };
