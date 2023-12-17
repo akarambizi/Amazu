4e2fa7e69924607a927ed95844e4085a10f91de6
Arthur Karambizi <arthurkarambizi2@gmail.com>
2023-12-16 23:27:11 -0800

    Update eslint rules and API URLs

--- client/src/api/services/utils.ts
+++ client/src/api/services/utils.ts
@@ -1,7 +1,21 @@
 /**
  * Gets the api url.
  * @param {string} path - The path to append to the base URL.
- * @param {string} [environment] - The environment to use. Defaults to "http://localhost:4200".
  * @returns {string} The api url.
  */
-export const getUrl = (path: string, environment?: string) => `http://localhost:4200${path}`;
+export const getUrl = (path: string) => {
+    let baseUrl;
+
+    switch (process.env.REACT_APP_API_ENV) {
+        case 'dev':
+            baseUrl = 'http://localhost:5000'; // MongoDB
+            break;
+        case 'mock':
+            baseUrl = 'http://localhost:4200'; // mockserver
+            break;
+        default:
+            baseUrl = ''; // prod
+    }
+
+    return `${baseUrl}${path}`;
+};
