470817c155d35453216e816ec6831a473a54df9c
Arthur Karambizi <arthurkarambizi2@gmail.com>
2023-12-29 01:52:58 -0800

    Update metricsMiddlewares and metrics.ts files

--- server/src/monitor/metrics.ts
+++ server/src/monitor/metrics.ts
@@ -3,12 +3,12 @@ import { MeterProvider } from '@opentelemetry/sdk-metrics';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { prometheusExporter } from './prometheus';
 
-const SERVICE_NAME = 'amazu-service-prometheus';
+export const SERVICE_NAME_PROMETHEUS = 'amazu-service-prometheus';
 
 // Create a new meter for the service;
 const meterProvider = new MeterProvider({
     resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: SERVICE_NAME,
+        [SemanticResourceAttributes.SERVICE_NAME]: SERVICE_NAME_PROMETHEUS,
     }),
 });
 
@@ -16,12 +16,15 @@ meterProvider.addMetricReader(prometheusExporter);
 const meter = meterProvider.getMeter('amazu-service-prometheus');
 
 // Create a new metric
-export const requestLatency = meter.createHistogram('http_request_latency', {
+export const requestLatency = meter.createCounter('duration_milliseconds_sum', {
     description: 'HTTP request latencies',
+    unit: 'ms',
 });
+
 // Create a new counter metric for request count
-export const requestCount = meter.createCounter('http_request_count', {
+export const requestCount = meter.createCounter('duration_milliseconds_count', {
     description: 'Count of total HTTP requests',
+    unit: '1',
 });
 
 // Create a new counter metric for error count
--- server/src/monitor/prometheus.ts
+++ server/src/monitor/prometheus.ts
@@ -6,4 +6,5 @@ const options: ExporterConfig = { port: 9464 };
 
 export const prometheusExporter = new PrometheusExporter(options, () => {
     logger.info(`Prometheus is live on http://localhost:${options.port}/metrics`);
+    logger.info(`Grafana is live on http://localhost:4000`);
 });
